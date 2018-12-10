aada5035801e43e3ede10f6c81cb40dc7b5cbaed
Michek11 <45753200+Michek11@users.noreply.github.com>
2018-12-10 08:19:30 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+function displayArticle(show1,hide1,hide2){
+	document.getElementById(show1).style.display="block";
+	document.getElementById(hide1).style.display="none";
+	document.getElementById(hide2).style.display="none";
+}
